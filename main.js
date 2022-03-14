152c19b3b4e2ecaa2346cce1d4fdb82c05c852ba
avellinwong01 <avellin.wong@gmail.com>
2022-03-15 05:19:18 +0800

    Finished L3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            // add new data propety 
+            image: './assets/images/socks_green.jpg', 
+            url: "https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3"
         }
     }
 })
