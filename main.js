e1592e878c0a5063fd643272bd95c84c3f64d25c
avellinwong01 <avellin.wong@gmail.com>
2022-03-15 04:58:45 +0800

    Finished L2

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({ // creates a Vue instance 
+    // pass in an object as a parameter (options object to add properties to configure app, 
+    // must be at least an empty object)
+    data() { // data property/option corresponding to a function -- ES6 shorthand 
+        return { // returns another object with product property as a data item  
+            product: 'Socks', 
+            description: 'I love socks'
+        }
+    }
+})
